e866b2f8278890291cca6b8919f133ebfeb68e33
daf <daf@madalien.com>
2018-07-06 23:26:23 +0100

    Made it clear it can be used for math * changed help text to make it clear you can use it as a calculator

--- droll/droll.py
+++ droll/droll.py
@@ -2,19 +2,23 @@ import discord
 from discord.ext import commands
 import xdice
 
-class DRoll:
-    """Rolls a dice expression"""
+# needed to improve autocomplete
+from redbot.core.bot import Red
+from discord.ext.commands import Context
 
-    def __init__(self, bot):
+class DRoll:
+    """Evaluates a dice expression"""
+
+    def __init__(self, bot: Red):
         self.bot = bot
 
 
     @commands.command(pass_context=True, aliases=["r"])
-    async def droll(self, ctx, exp: str):
-        """Rolls a set of dices."""
+    async def droll(self, ctx: Context, exp: str):
+        """Rolls a set of dices and/or evaluates a mathematical expression."""
 
         try:
             ps = xdice.roll(exp)
             await ctx.send("{} `{}` = ({}) = **{}**".format(ctx.author.mention, exp, ps.format(True), ps))
         except:
-            await ctx.send("Couldn't parse roll.")
+            await ctx.send("Error evaluating the given expression.")
